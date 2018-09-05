f612d6e576e7760e042fc93b4950824b279f8809
Aaron Redman <aaron.redman.15@cnu.edu>
2018-09-05 16:57:26 -0400

    Re-implemented file IO.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -13,43 +13,57 @@ public class Main {
 
     public static void main(String[] args) {
         String siteName;
+        String finalPassword;
+        String runAgain;
+        Boolean tryAgain = true;
+        int passwordLength = 14;
         int index;
         char temp;
-        char[] passChar = new char[14];
+        char[] passChar = new char[passwordLength];
         SecureRandom r = new SecureRandom();
         Scanner scan = new Scanner(System.in);
 
         try {
-            FileWriter writer = new FileWriter("passKeep.txt", true);
-            BufferedWriter bw = new BufferedWriter(writer);
+                FileWriter writer = new FileWriter("passKeep.txt", true);
+                BufferedWriter bw = new BufferedWriter(writer);
+            while (tryAgain) {
 
-            //populate char array for password
-            for (int i = 0; i < passChar.length-2; i++) {
+                //populate char array for password
+                for (int i = 0; i < passChar.length - 2; i++) {
                     passChar[i] = (alphabet.charAt(r.nextInt(alphabet.length())));
+                }
+
+                passChar[passChar.length - 2] = (numbers.charAt(r.nextInt(numbers.length())));
+                passChar[passChar.length - 1] = (specials.charAt(r.nextInt(specials.length())));
+
+                //Durstfeld shuffle
+                for (int i = 0; i < passChar.length; i++) {
+                    index = r.nextInt(passChar.length);
+                    temp = passChar[i];
+                    passChar[i] = passChar[index];
+                    passChar[index] = temp;
+                }
+
+                finalPassword = new String(passChar);
+
+                System.out.println("Enter the name of the site that you need a password for:");
+                siteName = scan.nextLine();
+                bw.write("Password for " + siteName + ":");
+                bw.newLine();
+                bw.write(finalPassword);
+                bw.newLine();
+
+                System.out.println("The password was written to PassKeep.txt\n\n" +
+                        "Would you like to generate another password? (Y/N)");
+
+                runAgain = scan.nextLine();
+                if (!(runAgain.equals("Y")) && !(runAgain.equals("y"))) {
+                    tryAgain = false;
+                }
+
             }
-
-            passChar[passChar.length-2] = (numbers.charAt(r.nextInt(numbers.length())));
-            passChar[passChar.length-1] = (specials.charAt(r.nextInt(specials.length())));
-
-            //Durstfeld shuffle
-            for (int i = 0; i < passChar.length; i++) {
-                index = r.nextInt(passChar.length);
-                temp = passChar[i];
-                passChar[i] = passChar[index];
-                passChar[index] = temp;
-            }
-
-            String passFinal = new String(passChar);
-
-            System.out.println("Enter the name of the site that you need a password for:");
-            siteName = scan.nextLine();
-            bw.write("Password for " + siteName + ":");
-            bw.newLine();
-            bw.write(passFinal);
-            bw.newLine();
-
-            bw.close();
-            writer.close();
+                bw.close();
+                writer.close();
 
 
         }
