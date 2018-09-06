aeac00b4f2360b850b3ee8b09bcb7dc4404145db
Aaron Redman <aaron.redman.15@cnu.edu>
2018-09-05 20:37:23 -0400

    Added ability to change password length (6-20 characters).

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -16,27 +16,41 @@ public class Main {
         String finalPassword;
         String runAgain;
         Boolean tryAgain = true;
-        int passwordLength = 14;
+        Integer passwordLength = 1;
         int index;
         char temp;
-        char[] passChar = new char[passwordLength];
+        char[] passChar;
+
         SecureRandom r = new SecureRandom();
         Scanner scan = new Scanner(System.in);
 
         try {
                 FileWriter writer = new FileWriter("passKeep.txt", true);
                 BufferedWriter bw = new BufferedWriter(writer);
+
             while (tryAgain) {
+
+                while ((passwordLength < 6) || (passwordLength > 20)) {
+                    System.out.println("Enter length of password (6-20).");
+                    passwordLength = Integer.valueOf(scan.nextLine());
+                }
+
+                //create password array of specified length
+                passChar = new char[passwordLength];
+
+                System.out.println("Enter the name of the site that you need a password for:");
+                siteName = scan.nextLine();
 
                 //populate char array for password
                 for (int i = 0; i < passChar.length - 2; i++) {
                     passChar[i] = (alphabet.charAt(r.nextInt(alphabet.length())));
                 }
 
+                //set last 2 indexes to a number and special char, respectively
                 passChar[passChar.length - 2] = (numbers.charAt(r.nextInt(numbers.length())));
                 passChar[passChar.length - 1] = (specials.charAt(r.nextInt(specials.length())));
 
-                //Durstfeld shuffle
+                //Durstfeld shuffle to mix up password
                 for (int i = 0; i < passChar.length; i++) {
                     index = r.nextInt(passChar.length);
                     temp = passChar[i];
@@ -44,10 +58,10 @@ public class Main {
                     passChar[index] = temp;
                 }
 
+                //convert password array into String
                 finalPassword = new String(passChar);
 
-                System.out.println("Enter the name of the site that you need a password for:");
-                siteName = scan.nextLine();
+
                 bw.write("Password for " + siteName + ":");
                 bw.newLine();
                 bw.write(finalPassword);
@@ -60,6 +74,7 @@ public class Main {
                 if (!(runAgain.equals("Y")) && !(runAgain.equals("y"))) {
                     tryAgain = false;
                 }
+                passwordLength = 1;
             }
                 bw.close();
                 writer.close();
